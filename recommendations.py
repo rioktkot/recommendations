acdb32f205b3a76dccccfd5ce1c2bd18d398e54a
rioktkot <136642767+rioktkot@users.noreply.github.com>
2025-06-04 12:09:19 +0300

    Update recommendations.py

--- recommendations.py
+++ recommendations.py
@@ -7,7 +7,7 @@ st.title(" Путеводитель по странам")
 st.markdown("""
     <style>
   .stMain {
-        background-color: white;
+        background-color: 74512D;
         padding: 20px;
         border-radius: 10px;
     }
